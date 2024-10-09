1f65c944705553a50dfb074c40570e8b1cc4a437
marlon.pichler@student.htl-rankweil.at <marlon.pichler@student.htl-rankweil.at>
2024-10-09 10:25:12 +0000

    Created Startseite and Database_Server; Edited settings; Deleted Form1

---
+++ client_code/Startseite/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import StartseiteTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+
+class Startseite(StartseiteTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    
+    # Any code you write here will run before the form opens.
+
+    self.drop_down_1.items = anvil.server.call('get_jugendherbergen')
