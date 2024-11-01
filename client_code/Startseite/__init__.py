53608b55f45b5d0cc28bb3df63c8caad364806f4
marlon.pichler@student.htl-rankweil.at <marlon.pichler@student.htl-rankweil.at>
2024-11-01 13:52:41 +0000

    Edited Startseite and Database_Server

--- client_code/Startseite/__init__.py
+++ client_code/Startseite/__init__.py
@@ -14,13 +14,16 @@ class Startseite(StartseiteTemplate):
     # Any code you write here will run before the form opens.
 
     self.drop_down_1.items = anvil.server.call('get_jugendherbergen', 'name,IDJugendherberge')
-    anvil.server.call('get_zimmer_for_jugendherberge', 1)
+    self.drop_down_2.items = anvil.server.call('get_zimmer_for_jugendherberge', 1)
+    self.dropdown_User.items = anvil.server.call('get_benutzer', 'Vorname,Nachname,Email')
+    self.dropdown_Preiskategorie.items = anvil.server.call('get_preiskategorien', 'Preis')
+    
 
   def drop_down_1_change(self, **event_args):
     """This method is called when an item is selected"""
     jid = self.drop_down_1.items\
     [self.drop_down_1.selected_value - 1][1]
-    anvil.server.call('get_zimmer_for_jugendherberge', jid)
-    print(jid)
+    self.drop_down_2.items = anvil.server.call('get_zimmer_for_jugendherberge', jid)
+    
     
     
--- server_code/Database_Server.py
+++ server_code/Database_Server.py
@@ -28,11 +28,48 @@ def get_jugendherbergen(rows='*'):
   return res
 
 @anvil.server.callable
-def get_zimmer_for_jugendherberge(id, columns="*"):
+def get_zimmer_for_jugendherberge(id, columns="MaxBettenanzahl,fkPreiskategorie"):
   conn = sqlite3.connect(data_files['buchungsdatenbank.db'])
   cursor = conn.cursor()
   res = list(cursor.execute(f'SELECT {columns} FROM tblZimmer WHERE fkJugendherberge = {id}'))
-  print(res)
-  return res
+
+  item_list = []
+  tmp_idx = 1
+  for item in res:
+    preis = list(cursor.execute(f'SELECT Preis FROM tblPreiskategorie WHERE IDPreiskategorie = {item[1]}'))
+    label = f"Zimmer mit {item[0]} Betten, Preis: {preis[0][0]}€"
+    tmp_tuple = (label,tmp_idx)
+    item_list.append(tmp_tuple)
+    tmp_idx += 1
+  
+  return item_list
+
+@anvil.server.callable
+def get_benutzer(columns="Vorname,Nachname,Email"):
+  conn = sqlite3.connect(data_files['buchungsdatenbank.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(f'SELECT {columns} FROM tblBenutzer'))
+  item_list = []
+  tmp_idx = 1
+  for item in res:
+    label = f"{item[0]} {item[1]} {item[2]}"
+    tmp_tuple = (label,tmp_idx)
+    item_list.append(tmp_tuple)
+    tmp_idx += 1
+  return item_list
+
+@anvil.server.callable
+def get_preiskategorien(id, columns="Preis"):
+  conn = sqlite3.connect(data_files['buchungsdatenbank.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(f'SELECT {columns} FROM tblPreiskategorie'))
+  item_list = []
+  tmp_idx = 1
+  for item in res:
+    label = f"{item[0]}€"
+    tmp_tuple = (label,tmp_idx)
+    item_list.append(tmp_tuple)
+    tmp_idx += 1
+  return item_list
 
 
