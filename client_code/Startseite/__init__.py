ecdeaac757c3795b7792de01d52b7f39bc7dda27
marlon.pichler@student.htl-rankweil.at <marlon.pichler@student.htl-rankweil.at>
2024-11-02 16:12:53 +0000

    Edited Startseite and Database_Server

--- client_code/Startseite/__init__.py
+++ client_code/Startseite/__init__.py
@@ -12,74 +12,43 @@ class Startseite(StartseiteTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     
-    self.hostel_id_index_relation = [] # [INDEX/ID] 
-    self.user_id_index_relation = []
-    self.price_id_index_relation = []
-    self.room_id_index_relation = []
-    self.extra_user_index = []
-    self.init_components(**properties)
-    self.loading_data()
+    # Laden der Daten aus der Datebank + Korrelation der IDs herstellen
+    
+    self.dropdown_user.items = anvil.server.call('get_all_users')
+    self.dropdown_jugendherberge.items = anvil.server.call('get_jugendherbergen')
+    self.dropdown_preiskategorie.items = anvil.server.call('get_preiskategorien')
+    self.dropdown_preiskategorie.selected_value = anvil.server.call('get_preiskategorie_for_user', self.dropdown_user.selected_value)
+    self.dropdown_zimmer.items = anvil.server.call('get_zimmer_for_jugendherberge',self.dropdown_jugendherberge.selected_value, self.dropdown_preiskategorie.selected_value)
+    self.dropdown_mitbucher.items = anvil.server.call('get_all_users',self.dropdown_user.selected_value)
+    self.dropdown_jugendherberge.items = anvil.server.call('get_jugendherbergen')
 
+  
   def dropdown_jugendherberge_change(self, **event_args):
-    """This method is called when an item is selected"""
-    print(self.dropdown_jugendherberge.selected_value)
-
-  def dropdown_user_change(self, **event_args):
-    """This method is called when an item is selected"""
-    pass
+    self.dropdown_zimmer.items = anvil.server.call('get_zimmer_for_jugendherberge', self.dropdown_jugendherberge.selected_value, self.dropdown_preiskategorie.selected_value)
 
   def dropdown_preiskategorie_change(self, **event_args):
-    """This method is called when an item is selected"""
+    self.dropdown_zimmer.items = anvil.server.call('get_zimmer_for_jugendherberge', self.dropdown_jugendherberge.selected_value, self.dropdown_preiskategorie.selected_value)
+    anvil.server.call('save_preiskategorie',self.dropdown_preiskategorie.selected_value, self.dropdown_user.selected_value)
+
+  def dropdown_user_change(self, **event_args):
     pass
 
-
-  def data_modify(self, data):
-    data_list = []
-    relation = []
-    count = 0
-    for i in data:
-      data_list.append([i[0],count]) # i[0] the Name  COUNT is the index in the dropdown
-      relation.append([count, i[1]]) # i [1] the ID  COUNT is the index in the dropdown
-      count +=1
-    return [data_list, relation]
-
-  def transform_index_to_id(self, data, index):
-    for i in data:
-      if index == i[0]:
-        return i[1]
- 
-  def loading_data(self):
-    self.load_Jugendherbergen()
-    self.load_user(self.dropdown_user)
-    self.load_user(self.dropdown_mitbucher)
-    self.load_price()
-    self.load_room()
-
-  def load_Jugendherbergen(self):
-    data_list = anvil.server.call('get_jugendherbergen')
-    data_modify = self.data_modify(data_list)
-    self.hostel_id_index_relation = data_modify[1]
-    self.dropdown_jugendherberge.items = data_modify[0]
-  def load_user(self, dropdown):
-    data_list = anvil.server.call('get_all_users')
-    data_modify = self.data_modify(data_list)
-    self.user_id_index_relation = data_modify[1]
-    self.dropdown_user.items = data_modify[0]
-
-  def load_price(self):
-    data_list = anvil.server.call('get_preiskategorien')
-    data_modify = self.data_modify(data_list)
-    self.price_id_index_relation = data_modify[1]
-    self.dropdown_preiskategorie.items = data_modify[0]
-
-  def load_room(self):
-    print(self.dropdown_jugendherberge.selected_value)
-    Jugendherberge_selected = self.transform_index_to_id(self.hostel_id_index_relation, self.dropdown_jugendherberge.selected_value)
-    Preiskategorie_selected = self.transform_index_to_id(self.price_id_index_relation, self.dropdown_preiskategorie.selected_value)
-    data_list = anvil.server.call('get_zimmer_for_jugendherberge',Jugendherberge_selected, Preiskategorie_selected)
-    data_modify = self.data_modify(data_list)
-    self.room_id_index_relation = data_modify[1]
-    self.dropdown_zimmer.items = data_modify[0] 
-
-
+  def button_AddUser_click(self, **event_args):
+    l = Link(text = anvil.server.call('get_user', self.dropdown_mitbucher.selected_value))
+    l.icon="fa:times"
+    l.icon_align="left"
+    l.background="#eee"
+    l.role="lozenge"
+    l.border="1px solid #888"
+    l.set_event_handler("click",self.delete)
+    for item in self.flowpanel_additionalUser:
+      if (l == item):
+        return
+    self.flowpanel_additionalUser.add_component(l)
+    
+  def delete(self,**k):
+    print("clicked by :",k['sender'].text)
+    k['sender'].remove_from_parent()
+    
+  
 
--- server_code/Database_Server.py
+++ server_code/Database_Server.py
@@ -20,20 +20,20 @@ def get_jugendherbergen():
   conn = sqlite3.connect(data_files['buchungsdatenbank.db'])
   cursor = conn.cursor()
   res = list(cursor.execute('SELECT Name,IDJugendherberge FROM tblJugendherberge'))
-  print(res)
   return res
 
 @anvil.server.callable
-def get_all_users():
+def get_all_users(ID=0):
+  print(ID, "Hallo")
   conn = sqlite3.connect(data_files['buchungsdatenbank.db'])
   cursor = conn.cursor()
   res = list(cursor.execute(
       '''
       SELECT Vorname || " " || Nachname || " " || Email AS label, IDBenutzer
-      FROM tblBenutzer
-      '''
-  ))
-  print(res)
+      FROM tblBenutzer WHERE IDBenutzer != ?
+      ''',
+      (str(ID))
+  )) 
   return res
 
 @anvil.server.callable
@@ -48,6 +48,22 @@ def get_preiskategorien():
   ))
   print(res)
   return res
+@anvil.server.callable
+def get_preiskategorie_for_user(ID):
+  conn = sqlite3.connect(data_files['buchungsdatenbank.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(
+      '''
+      SELECT IDPreiskategorie
+      FROM tblPreiskategorie 
+      WHERE IDPreiskategorie = ?
+      ''',
+    (str(ID))
+    
+  ))
+  item = res[0][0]
+  print(item)
+  return item
   
 @anvil.server.callable
 def get_zimmer_for_jugendherberge(JID, PK_current):
@@ -59,7 +75,35 @@ def get_zimmer_for_jugendherberge(JID, PK_current):
       FROM tblZimmer 
       WHERE fkJugendherberge = ? AND fkPreiskategorie = ?
       ''', 
-      (JID, PK_current)
+      (str(JID), str(PK_current))
   ))
-  print(res)
   return res
+
+@anvil.server.callable
+def save_preiskategorie(PID,UserID):
+  # theoretisch speichert er die neue Preiskategorie hier ab, aber shit not working
+  conn = sqlite3.connect(data_files['buchungsdatenbank.db'])
+  cursor = conn.cursor()
+  cursor.execute(
+      '''
+      UPDATE tblBenutzer
+      SET fkPreiskategorie = ?
+      WHERE IDBenutzer = ?
+      ''', 
+      (PID, UserID)
+  )
+@anvil.server.callable
+def get_user(ID):
+  conn = sqlite3.connect(data_files['buchungsdatenbank.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(
+    '''
+    SELECT Vorname || " " || Nachname
+    FROM tblBenutzer
+    WHERE IDBenutzer = ?
+    ''', 
+    (str(ID))
+    ))
+  print(res[0][0])
+  return str(res[0][0])
+  
